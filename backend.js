e1446c3dc21b1049c13ff9340fbbdbb4251afc4a
Pyry Kröger <pyry@krogerit.net>
2013-06-15 00:24:27 +0300

    added support for logging location history

--- backend.js
+++ backend.js
@@ -13,18 +13,23 @@ app.use(express.static(__dirname + '/client')); // Serve static files from publi
 // {"id": "abc", "lat": 12.34, "lng": 56.78, 100}
 app.post('/api/location', function(req, res) {
 	console.log('POST location request:', req.body, ' - ', typeof req.body);
-	console.log(req);
 	var data = req.body;
 	data.timestamp = new Date();
 	
-	db.collection('locations').update({id: data.id}, data, {safe: true, upsert: true}).done(function(success) {
+	db.collection('locations').insert(data, {safe: true}).done(function(success) {
 		res.json({success: success}, success ? 200 : 404);
 	});
 });
 
 app.get('/api/location', function(req, res) {
 	db.collection('locations').find().toArray().done(function(locations) {
-        res.json(locations);
+		var people = _.groupBy(locations, function(it) { return it.id });
+		var newest = _.map(people, function(it) {
+			return _.max(it, function(it) {
+				return it.timestamp;
+			});
+		});
+        res.json(newest);
     });
 });
 
